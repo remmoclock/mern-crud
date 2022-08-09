91ebc4ed76d6bc24a8b5efa2ac19f4deb4a682a0
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-08-09 21:56:46 +0200

    connect mongoose

--- api/server.js
+++ api/server.js
@@ -1,9 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require("cors");
+require('dotenv').config();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.use(cors());
+
+mongoose
+  .connect(`mongodb+srv://mido:${process.env.MONGO_PASSWORD}@cluster0.1suzkrt.mongodb.net/test`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(console.error);
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
 
 app.listen(PORT, () => console.log("Server run on port " + PORT));
