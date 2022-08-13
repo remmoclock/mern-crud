4424b31e5236efb565e8b2918a64588f931718fe
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-08-13 15:44:27 +0200

    Add post request

--- api/models/Todo.js
+++ api/models/Todo.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const TodoSchema = new Schema({
-	text: {
-		type: String,
-		required: true
-	},
-	complete: {
-		type: Boolean,
-		default: false
-	},
-	timestamp: {
-		type: String,
-		default: Date.now()
-	}
+  text: {
+    type: String,
+    required: true,
+  },
+  complete: {
+    type: Boolean,
+    default: false,
+  },
+  timestamp: {
+    type: String,
+    default: Date.now(),
+  },
 });
 
 const Todo = mongoose.model("Todo", TodoSchema);
--- api/server.js
+++ api/server.js
@@ -3,20 +3,40 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose
-  .connect(`mongodb+srv://mido:${process.env.MONGO_PASSWORD}@cluster0.1suzkrt.mongodb.net/test`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    `mongodb+srv://mido:${process.env.MONGO_PASSWORD}@cluster0.1suzkrt.mongodb.net/test`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("Connected to MongoDB"))
   .catch(console.error);
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+// Models
+const Todo = require("./models/Todo");
+
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+
+  res.json(todos);
+});
+
+app.post("/todo/new", (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
+
+  todo.save();
+
+  res.json(todo);
 });
 
 app.listen(PORT, () => console.log("Server run on port " + PORT));
