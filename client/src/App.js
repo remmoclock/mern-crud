80159d3ecfcb7bbf1157367124dca3720717fb40
remmoclock <59166652+remmoclock@users.noreply.github.com>
2022-08-14 00:54:41 +0200

    set data & some css

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      MERN TODO
+      <h1>Bonjour</h1>
+      <h4>Tâches</h4>
+      <div className="todos">
+        <div className="todo">
+          <div className="checkbox"></div>
+          <div className="text">Faire le Ménage</div>
+          <div className="delete-todo">x</div>
+        </div>
+        <div className="todo is-complete">
+          <div className="checkbox"></div>
+          <div className="text">Rdv médecin</div>
+          <div className="delete-todo">x</div>
+        </div>
+      </div>
     </div>
   );
 }
